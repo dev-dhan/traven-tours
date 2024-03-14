29e570afbf31dd168eed00b83844131c33478590
rafdev133 <rafaelkira133@gmail.com>
2024-03-14 15:11:21 +0800

    march 14 webtask

---
+++ script/script.js
@@ -0,0 +1,7 @@
+
+  document.querySelector(".btn-agree").addEventListener("click", function() {
+    document.querySelector(".thankyou_modal").style.display = "block";
+    document.querySelector(".modal").style.display = "none";
+  });
+
+ 
